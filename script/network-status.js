0fab88bc7a42d64d5602a87f7a89b6b7b4766113
windlessstrome <aakash.koli@mitel.com>
2020-10-28 14:02:55 +0530

    third commit

--- script/network-status.js
+++ script/network-status.js
@@ -1,34 +1,33 @@
 (function () {
 	setTimeout(function () {
-		document.getElementById('reload-button').addEventListener('click', function() {
-			document.getElementById('reload-popup').style.display = 'none';
+		$('#reload-button').on('click', function() {
+			$('#reload-popup').css('display', 'none');
 			nw.App.clearCache();
 			win.reload();
 		})
-	}, 1000)
+	}, 2000)
 
 	setInterval(async () => {
-		fetch('https://www.mitel.com/', {
-			method: 'HEAD',
-			mode: 'no-cors',
-			cache: 'no-cache'
-		})
-		.then(response => {
+		try {
+			let response = await fetch('https://www.mitel.com/', {
+				method: 'HEAD',
+				mode: 'no-cors',
+				cache: 'no-cache'
+			})
 			console.log(`Network check passed: ${response}`);
-			document.getElementById('network-status').style.display = 'none';
+			$('#network-status').css('display', 'none');
 			setTimeout(function() {
-				if (!document.getElementById('a-profile')) {
-					document.getElementById('reload-popup').style.display = 'flex';
+				if (!$('#a-profile')) {
+					$('#reload-popup').css('display', 'flex');
 				}
-			}, 12000)
-		})
-		.catch(error => {
+			}, 7000)
+		} catch(error) {
 			console.log(`Network check failed: ${error}`);
-			if (document.getElementById('a-profile')) return;
-			if (document.getElementById('spinner')) {
-				document.getElementById('spinner').style.display = 'none';
+			if ($('#a-profile')) return;
+			if ($('#spinner')) {
+				$('#spinner').css('display', 'none');
 			}
-			document.getElementById('network-status').style.display = 'block';
-		});
-	}, 15000)
+			$('#network-status').css('display', 'block');
+		}
+	}, 10000)
 })();
