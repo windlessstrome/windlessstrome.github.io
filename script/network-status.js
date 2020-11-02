365516e9b05cbd4088e2e87f60934019de861e2c
windlessstrome <aakash.koli@mitel.com>
2020-11-02 12:18:41 +0530

    revert

--- script/network-status.js
+++ script/network-status.js
@@ -1,11 +1,10 @@
 (function () {
-		setTimeout(function() {
-			document.getElementById('reload-button').addEventListener('click', function() {
-				document.getElementById('reload-popup').style.display = "none";
-				nw.App.clearCache();
-				win.reload();
-			})
-		}, 2000);
+	$(document).ready(function() {
+		$('#reload-button').on('click', function() {
+			$('#reload-popup').hide();
+			nw.App.clearCache();
+			win.reload();
+		})
 	
 		setInterval(async () => {
 			try {
@@ -15,22 +14,20 @@
 					cache: 'no-cache'
 				})
 				console.log(`Network check passed: ${response}`);
-				document.getElementById('network-status').style.display = "none";
+				$('#network-status').hide();
 				setTimeout(function() {
 					if (!document.getElementById('a-profile')) {
-						document.getElementById('reload-popup').style.display = "flex";
+						$('#reload-popup').css("display", "flex");
 					}
-				}, 1000)
+				}, 7000)
 			} catch(error) {
 				console.log(`Network check failed: ${error}`);
 				if (document.getElementById('a-profile')) return;
 				if (document.getElementById('spinner')) {
-					document.getElementById('spinner').style.display = "none";
+					$('#spinner').hide();
 				}
-				if (document.querySelector('st-window')) {
-					document.getElementById('st-window').style.display = "none";
-				}
-				document.getElementById('network-status').style.display = "block";
+				$('#network-status').show();
 			};
 		}, 10000)
+	})
 })();
